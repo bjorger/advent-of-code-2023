f6b25ab7e1f0dc865d0090c5b3ad9ea26220a9fa
Robin Braumann <robin.braumann@softwaredefinedautomation.io>
2023-12-19 14:33:27 +0100

    Finish first part of day 2

--- Day_2/main.py
+++ Day_2/main.py
@@ -11,7 +11,7 @@ amount_red = 12
 amount_green = 13
 
 
-def split_line_into_colors(line: str) -> int:
+def find_possible_games(line: str) -> int:
     if len(line) == 0:
         return 0
 
@@ -44,5 +44,43 @@ def split_line_into_colors(line: str) -> int:
     return int(game_index)
 
 
-print(sum([split_line_into_colors(line) for line in data]))
+print(sum([find_possible_games(line) for line in data]))
 
+
+def find_power_of_fewest_cubes_possible(line: str) -> int:
+    if len(line) == 0:
+        return 0
+
+    relevant_information = line.split(":")
+    games = relevant_information[1].split(";")
+
+    blue = 0
+    red = 0
+    green = 0
+
+    for game in games:
+        split_game = game.split(",")
+        for split in split_game:
+            blue_index = split.find("blue")
+            red_index = split.find("red")
+            green_index = split.find("green")
+
+            if blue_index != -1:
+                new_blue = int(split.replace("blue", "").replace(" ", ""))
+                if blue < new_blue:
+                    blue = new_blue
+
+            if red_index != -1:
+                new_red = int(split.replace("red", "").replace(" ", ""))
+                if red < new_red:
+                    red = new_red
+
+            if green_index != -1:
+                new_green = int(split.replace("green", "").replace(" ", ""))
+                if green < new_green:
+                    green = new_green
+
+    return blue * red * green
+
+
+print(sum([find_power_of_fewest_cubes_possible(line) for line in data]))
